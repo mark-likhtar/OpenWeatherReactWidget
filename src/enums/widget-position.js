0de57fe5efff0c26f6157898b0906f5f469cd7e4
marik0201 <marktechart@gmail.com>
2019-06-06 09:46:50 +0300

    remove icon font, add svg icons, create enum for icons

---
+++ src/enums/widget-position.js
@@ -0,0 +1,6 @@
+export default {
+    'top-left': "weather-widget__top-left",
+    'top-right': "weather-widget__top-right",
+    'bottom-left': "weather-widget__bottom-left",
+    'bottom-right': "weather-widget__bottom-right"
+}
